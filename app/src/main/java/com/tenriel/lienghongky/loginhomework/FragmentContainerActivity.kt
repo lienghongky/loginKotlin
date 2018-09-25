c74deb5a08070c23050f5ec58524299eb19d8220
CKCC-IMac17 <ckcc-imac17@CKCCs-iMac.local>
2018-09-25 19:15:23 +0700

    add clouser

---
+++ app/src/main/java/com/tenriel/lienghongky/loginhomework/FragmentContainerActivity.kt
@@ -0,0 +1,69 @@
+package com.tenriel.lienghongky.loginhomework
+
+import android.app.Fragment
+import android.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.tenriel.lienghongky.loginhomework.fragments.OneFragment
+import com.tenriel.lienghongky.loginhomework.fragments.ThreeFragment
+import com.tenriel.lienghongky.loginhomework.fragments.TwoFragment
+import kotlinx.android.synthetic.main.activity_fragment_container.*
+import kotlinx.android.synthetic.main.fragment_one.*
+import kotlinx.android.synthetic.main.fragment_three.*
+import kotlinx.android.synthetic.main.fragment_two.*
+
+class FragmentContainerActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        btnNex.callOnClick()
+    }
+
+    var fragments:List<android.support.v4.app.Fragment>? = null
+     var i = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_container)
+        fragments = listOf(
+                OneFragment(),
+                TwoFragment(),
+                ThreeFragment()
+        )
+        val click:((View)->Unit) = {
+            btnNex.callOnClick()
+            (it as Button).text = "ok kha"
+        }
+        (this.fragments!![0] as OneFragment).onClick = click
+        (this.fragments!![1] as TwoFragment).onClick = click
+        (this.fragments!![2] as ThreeFragment).onClick = click
+
+
+
+        moveFragments(i)
+    btnNex.setOnClickListener {
+        i++
+        if (i > fragments!!.size-1){
+            i=0
+        }
+        moveFragments(i)
+    }
+        btnPre.setOnClickListener {
+
+                i--
+                if (i < 0){
+                    i=fragments!!.size-1
+                }
+                moveFragments(i)
+
+        }
+
+
+    }
+
+     fun moveFragments(index:Int){
+         var fragmentManager = supportFragmentManager
+                 .beginTransaction()
+                 .replace(R.id.fragment_container, fragments!![index])
+                 .commit()
+     }
+}
---
+++ app/src/main/java/com/tenriel/lienghongky/loginhomework/fragments/OneFragment.kt
@@ -0,0 +1,78 @@
+package com.tenriel.lienghongky.loginhomework.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import com.tenriel.lienghongky.loginhomework.R
+import kotlinx.android.synthetic.main.fragment_one.*
+
+
+class OneFragment : Fragment() {
+    var onClick:((View)->Unit)? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+
+        var btn = view.findViewById<Button>(R.id.btn1)
+        btn.setOnClickListener {
+            onClick!!(btn)
+
+        }
+
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+    }
+
+
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        //Find the +1 button
+
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+    }
+
+
+    companion object {
+        private val ARG_PARAM1 = "param1"
+        private val ARG_PARAM2 = "param2"
+        // The request code must be 0 or greater.
+        private val PLUS_ONE_REQUEST_CODE = 0
+
+
+        fun newInstance(param1: String, param2: String): PlusOneFragment {
+            val fragment = PlusOneFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+}
+
